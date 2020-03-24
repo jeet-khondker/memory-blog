1e8c27a47ca9aa38a9f6ba53fdb3da7959ba2b60
Jeet Khondker <jeet.usolutionsgroup@gmail.com>
2020-03-25 06:31:43 +0900

    Templating

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from app import app
+from flask import render_template, url_for
 
 # Dashboard Route
 @app.route('/')
 @app.route("/dashboard")
 def dashboard():
-    return "User Dashboard"
+    return render_template("dashboard.html", title = "Dashboard")
