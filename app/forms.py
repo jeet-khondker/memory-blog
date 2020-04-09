5d35c95b71b0ee994ce130dc3bb3d3b13974314d
Jeet Khondker <jeet.usolutionsgroup@gmail.com>
2020-04-10 05:29:34 +0900

    Added Update Account Design & Functionality

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, DateField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
-from wtforms.fields.html5 import DateField
+# from wtforms.fields.html5 import DateField
 from app.models import User
 
 # User Login Form
@@ -18,7 +19,7 @@ class RegistrationForm(FlaskForm):
     username = StringField("Username", validators = [DataRequired(message = "Username is required.")], render_kw = {"placeholder": "Username"})
     firstname = StringField("First Name", validators = [DataRequired(message = "First Name is required.")], render_kw = {"placeholder": "First Name"})
     middlename = StringField("Middle Name", render_kw = {"placeholder": "Middle Name"})
-    lastname = StringField("Last Name", validators = [DataRequired(message = "Last Name is required.")], render_kw = {"placeholder": "First Name"})
+    lastname = StringField("Last Name", validators = [DataRequired(message = "Last Name is required.")], render_kw = {"placeholder": "Last Name"})
     dob = DateField("Date Of Birth", validators = [DataRequired(message = "Date Of Birth is required.")], format = "%Y-%m-%d")
     email = StringField("Email", validators = [DataRequired(message = "Email Address is required.")], render_kw = {"placeholder": "Email Address"})
     password = PasswordField("Password", validators = [DataRequired(message = "Password is required.")], render_kw = {"placeholder": "Password"})
@@ -34,6 +35,29 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email = email.data).first()
         if user is not None:
             raise ValidationError("Email Address Already Exists! Please Use A Different Email Address.")
+
+# User Registration Form
+class UpdateAccountForm(FlaskForm):
+    username = StringField("Username", validators = [DataRequired(message = "Username is required.")], render_kw = {"placeholder": "Username"})
+    firstname = StringField("First Name", validators = [DataRequired(message = "First Name is required.")], render_kw = {"placeholder": "First Name"})
+    middlename = StringField("Middle Name", render_kw = {"placeholder": "Middle Name"})
+    lastname = StringField("Last Name", validators = [DataRequired(message = "Last Name is required.")], render_kw = {"placeholder": "Last Name"})
+    dob = DateField("Date Of Birth", validators = [DataRequired(message = "Date Of Birth is required.")], format = "%Y-%m-%d")
+    email = StringField("Email", validators = [DataRequired(message = "Email Address is required.")], render_kw = {"placeholder": "Email Address"})
+    photo = FileField("Profile Photo", validators = [FileAllowed(["jpg", "png"])])
+    submit = SubmitField("Update Account")
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+            if user is not None:
+                raise ValidationError("Username Already Exists! Please Use A Different Username.")
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email = email.data).first()
+            if user is not None:
+                raise ValidationError("Email Address Already Exists! Please Use A Different Email Address.")
 
 class PostForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired(message = "Post Title is required.")], render_kw = {"placeholder": "Post Title"})
--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
+import os, secrets
 from app import app, db
 from PIL import Image
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
-from app.forms import LoginForm, RegistrationForm, PostForm
+from app.forms import LoginForm, RegistrationForm, PostForm, UpdateAccountForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Post
 from datetime import datetime
@@ -20,6 +21,7 @@ def dashboard():
     photo = url_for('static', filename = 'profile_photos/' + current_user.photo)
     return render_template("dashboard.html", photo = photo, title = "Dashboard", posts = posts)
 
+# Login Route
 @app.route("/login", methods = ["GET", "POST"])
 def login():
     if current_user.is_authenticated:
@@ -37,11 +39,13 @@ def login():
         return redirect(next_page)
     return render_template("login.html", title = "Login", form = form)
 
+# Logout Route
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for("dashboard"))
 
+# User Registration Route
 @app.route("/register", methods = ["POST", "GET"])
 def register():
     if current_user.is_authenticated:
@@ -56,13 +60,49 @@ def register():
         return redirect(url_for("login"))
     return render_template("register.html", title = "User Registration", form = form)
 
-@app.route("/user/<username>")
-@login_required
-def user(username):
-    user = User.query.filter_by(username = username).first_or_404()
-    photo = url_for('static', filename = 'profile_photos/' + current_user.photo)
-    return render_template("account.html", user = user, photo = photo, title = "Account of {} {} {}".format(current_user.firstname, current_user.middlename, current_user.lastname))
+# Save Photo Route
+def save_photo(form_photo):
+    random_hex = secrets.token_hex(8)
+    _, fileExtension = os.path.splitext(form_photo.filename)
+    photo_filename = random_hex + fileExtension
+    photo_path = os.path.join(app.root_path, "static/profile_photos", photo_filename)
 
+    output_size = (125, 125)
+    img = Image.open(form_photo)
+    img.thumbnail(output_size)
+    img.save(photo_path)
+
+    return photo_filename
+
+# Account Information Route
+@app.route("/user", methods = ["GET", "POST"])
+@login_required
+def user():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.photo.data:
+            photo_file = save_photo(form.photo.data)
+            current_user.photo = photo_file
+        current_user.username = form.username.data
+        current_user.firstname = form.firstname.data
+        current_user.middlename = form.middlename.data
+        current_user.lastname = form.lastname.data
+        current_user.dob = form.dob.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your Account Has Been Successfully Updated!", "success")
+        return redirect(url_for("user"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.firstname.data = current_user.firstname
+        form.middlename.data = current_user.middlename
+        form.lastname.data = current_user.lastname
+        form.dob.data = current_user.dob
+        form.email.data = current_user.email
+    photo = url_for('static', filename = 'profile_photos/' + current_user.photo)
+    return render_template("account.html", user = user, photo = photo, form = form, title = "Account of {} {} {}".format(current_user.firstname, current_user.middlename, current_user.lastname))
+
+# Post Information Route
 @app.route("/post/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id)
