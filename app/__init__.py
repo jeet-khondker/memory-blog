7f832d25681f264a75c2b743559c877b65927727
Jeet Khondker <jeet.usolutionsgroup@gmail.com>
2020-05-17 12:04:16 +0900

    Added File Logging Functionality

--- app/__init__.py
+++ app/__init__.py
@@ -45,7 +45,7 @@ if not app.debug:
         if not os.path.exists("logs"):
             os.mkdir("logs")
         
-        file_handler = RotatingFileHandler("logs/memoryblog.log", maxBytes = 10240, backupCount = 10)
+        file_handler = RotatingFileHandler("logs/memoryblog.log")
         file_handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"))
         file_handler.setLevel(logging.INFO)
         app.logger.addHandler(file_handler)
