99bc7f29e2a73b2bc80826cb44099ea56b6f7773
Jeet Khondker <jeet.usolutionsgroup@gmail.com>
2020-05-31 12:01:10 +0900

    Added 403 Access Denied Page Functionality

--- app/errors.py
+++ app/errors.py
@@ -3,6 +3,10 @@
 from flask import render_template
 from app import app, db
 
+@app.errorhandler(403)
+def access_denied_error(error):
+    return render_template("403.html"), 403
+
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template("404.html"), 404
